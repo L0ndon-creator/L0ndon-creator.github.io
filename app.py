abe8f463d5c5af66af1ce55d221d909113e7424e
L0ndon-creator <74373122+L0ndon-creator@users.noreply.github.com>
2026-05-12 14:13:25 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@app.route('/recommendations')
+def recommendations():
+    return render_template('recommendations.html')
+
+@app.route('/gosts')
+def gosts():
+    return render_template('gosts.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
